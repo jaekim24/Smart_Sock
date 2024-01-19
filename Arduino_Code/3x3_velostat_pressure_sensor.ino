cc0c271899ac5fcc81ecd97b087774b646c96235
Jae <kim_jae1@students.columbusstate.edu>
2024-01-18 20:27:10 -0500

    code for 3x3 velostat pressure sensor

---
+++ Arduino_Code/3x3_velostat_pressure_sensor.ino
@@ -0,0 +1,56 @@
+// rows: for each row connect to 1k ohm resistors which all connects to the ground
+// rows: also connect each to the analog pins
+// cols: connect each to the digital pins 
+
+#define numRows 3
+#define numCols 3
+#define sensorPoints numRows*numCols
+
+int rows[] = {A0, A1, A2};
+int cols[] = {5,6,7};
+int incomingValues[sensorPoints] = {};
+
+void setup() {
+// set all rows and columns to INPUT (high impedance):
+  for (int i = 0; i < numRows; i++) 
+  {
+    pinMode(rows[i], INPUT_PULLUP);
+  }
+  for (int i = 0; i < numCols; i++) 
+  {
+    pinMode(cols[i], INPUT);
+  }
+  
+  Serial.begin(9600);
+}
+
+void loop() 
+  {
+  
+  for (int colCount = 0; colCount < numCols; colCount++) 
+     {
+      pinMode(cols[colCount], OUTPUT); // set as OUTPUT
+      digitalWrite(cols[colCount], LOW); // set LOW
+
+        for (int rowCount = 0; rowCount < numRows; rowCount++) 
+          {
+            incomingValues[colCount * numRows + rowCount] = analogRead(rows[rowCount]); // read INPUT
+          }// end rowCount
+
+      pinMode(cols[colCount], INPUT); // set back to INPUT!
+
+     }// end colCount
+
+  // Print the incoming values of the grid:
+  for (int i = 0; i < sensorPoints; i++) 
+  {
+    Serial.print(incomingValues[i]);
+    if (i < sensorPoints - 1)
+    {
+      Serial.print("    ");
+    }
+    
+   }
+  Serial.println();
+  delay(10);
+  }
