be081116e32bd46af6c3754da0f390649bc8cb1a
Jae <kim_jae1@students.columbusstate.edu>
2024-01-19 15:32:58 -0500

    update

--- code/8x8_velostat_pressure_sensor/code/8x8_pressure_sensor/8x8_pressure_sensor.ino
+++ code/8x8_velostat_pressure_sensor/code/8x8_pressure_sensor/8x8_pressure_sensor.ino
@@ -12,12 +12,12 @@ HIGH, LOW, or INPUT (high impedance) to detect location and pressure.
 >> http://howtogetwhatyouwant.at/
 */
 
-#define numRows 7
-#define numCols 7
+#define numRows 8
+#define numCols 8
 #define sensorPoints numRows * numCols
 
-int rows[] = {A0, A1, A2, A3, A4, A5, A6};
-int cols[] = {11, 10, 9, 8, 7, 6, 5};
+int cols[] = {A0, A1, A2, A3, A4, A5, A6,A7};
+int rows[] = {2,3,4,5,6,7,8,9};
 int incomingValues[sensorPoints] = {};
 
 void setup() {
@@ -48,9 +48,11 @@ void loop() {
   for (int i = 0; i < sensorPoints; i++) {
     Serial.print(incomingValues[i]);
     if (i < sensorPoints - 1)
-      Serial.print("\t");
+      Serial.print("   "); // doesnt append space after last number 
+    if (i == 7 || (i-7) % 8 == 0 ){
+      Serial.println();        // prints one row at a time 
+    }
   }
   Serial.println();
   delay(10);
 }
-
